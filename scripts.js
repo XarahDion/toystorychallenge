b42a9a3ba98fd1deb09bbc3b13d77a695a263158
XarahDion <96599351+XarahDion@users.noreply.github.com>
2022-06-22 17:32:06 -0400

    june22

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
 console.log("To infinity and beyond.");
 
-// write your JavaScript here
+const myForm = document.querySelector("#myForm");
+const commentsInput = document.querySelector("#comments");
+const ratingsList = document.querySelector("#ratings");
+
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+    const li = document.createElement("li");
+
+    li.appendChild(document.createTextNode(`${commentsInput.value}`));
+    ratingsList.appendChild(li);
+    li.innerHTML = `" ${commentsInput.value} "`;
+    commentsInput.value = "";
+}
